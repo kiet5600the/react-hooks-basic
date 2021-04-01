db996712d5b5d74097af445053cf3def1cc7c436
kietthe <kiet5600the@gmail.com>
2021-04-01 11:31:40 +0700

    get api with useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import PostList from './components/PostList';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
@@ -32,11 +33,31 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1'
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        const { data } = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log("error fetch post list: ", error.message)
+
+      }
+    }
+
+    fetchPostList()
+  }, [])
+
   return (
     <div className="app">
       <h1>Welcome to React Hooks!</h1>
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
+      {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+      <PostList posts={postList} />
     </div>
   );
 }
